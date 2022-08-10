f222c972a5848a2ce611df349007d245b28c270d
Moritz Hoffmann <clonker@gmail.com>
2022-08-10 13:02:16 +0200

    Small improvements.

--- examples/bench.cpp
+++ examples/bench.cpp
@@ -17,17 +17,18 @@ Generator randomlySeededGenerator() {
 
 int main() {
 
-    std::uint32_t nJobs {8};
+    std::uint32_t nJobs{8};
 
     using Vec3 = neighborlists::Vec<double, 3>;
+    Vec3 boxSize{10., 10., 10.};
 
     std::size_t nParticles = 20000;
-    std::vector<Vec3> positions (nParticles);
+    std::vector<Vec3> positions(nParticles);
     {
         auto generator = randomlySeededGenerator();
-        for (std::size_t i = 0; i < nParticles; ++i) {
-            for (std::size_t d = 0; d < Vec3::dim; ++d) {
-                std::uniform_real_distribution<Vec3::value_type> dist{-5., 5.};
+        for (std::size_t d = 0; d < Vec3::dim; ++d) {
+            std::uniform_real_distribution<Vec3::value_type> dist{-boxSize[d] / 2, boxSize[d] / 2};
+            for (std::size_t i = 0; i < nParticles; ++i) {
                 positions[i][d] = dist(generator);
             }
         }
@@ -35,9 +36,7 @@ int main() {
 
     std::atomic<std::uint32_t> pairs;
     {
-        std::array<Vec3::value_type, Vec3::dim> boxSize;
-        std::fill(begin(boxSize), end(boxSize), 10.);
-        neighborlists::CellLinkedList<Vec3::dim, false, Vec3::value_type> cll{boxSize, .5};
+        neighborlists::CellLinkedList<Vec3::dim, false, Vec3::value_type> cll{boxSize.data, .5};
         auto start = std::chrono::high_resolution_clock::now();
         cll.update(begin(positions), end(positions), nJobs);
         cll.forEachParticlePair([&positions, &pairs](auto i, auto j) {
@@ -47,7 +46,8 @@ int main() {
         }, nJobs);
         auto end = std::chrono::high_resolution_clock::now();
         auto elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
-        spdlog::error("Elapsed for CLL {}ms = {} * {}ms", elapsed.count(), nJobs, elapsed.count() / static_cast<float>(nJobs));
+        spdlog::error("Elapsed for CLL {}ms = {} * {}ms", elapsed.count(), nJobs,
+                      elapsed.count() / static_cast<float>(nJobs));
     }
 
     std::uint32_t referencePairs{};
@@ -62,7 +62,8 @@ int main() {
         }
         auto end = std::chrono::high_resolution_clock::now();
         auto elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
-        spdlog::error("Elapsed for reference {}ms = {} * {}ms", elapsed.count(), nJobs, elapsed.count() / static_cast<float>(nJobs));
+        spdlog::error("Elapsed for reference {}ms = {} * {}ms", elapsed.count(), nJobs,
+                      elapsed.count() / static_cast<float>(nJobs));
     }
 
     return 0;
--- test/CellLinkedListTest.cpp
+++ test/CellLinkedListTest.cpp
@@ -4,23 +4,63 @@
 
 #include <neighborlists/Vec.h>
 #include <neighborlists/CellLinkedList.h>
+#include <random>
 
 
 TEST_CASE("CellLinkedListSanity", "[cell_linked_list]") {
     using Vec1 = neighborlists::Vec<double, 1>;
     neighborlists::CellLinkedList<Vec1::dim, false, double> cll{{10.}, 1.};
-    std::vector<Vec1> positions {
-            {{-4.5}}, {{-3.4}}, {{3.}}  // only particles 0 and 1 are neighbors of one another
+    std::vector<Vec1> positions{
+            {{-4.5}},
+            {{-3.4}},
+            {{3.}}  // only particles 0 and 1 are neighbors of one another
     };
     cll.update(begin(positions), end(positions), 2);
 
-    std::vector<std::tuple<int, int>> pairs {};
+    std::vector<std::tuple<int, int>> pairs{};
     std::mutex m;
     cll.forEachParticlePair([&pairs, &m](auto i, auto j) {
-        std::scoped_lock lock {m};
+        std::scoped_lock lock{m};
         pairs.push_back(std::make_tuple(i, j));
     }, 2);
 
     REQUIRE(std::find(begin(pairs), end(pairs), std::make_tuple(0, 1)) != end(pairs));
     REQUIRE(std::find(begin(pairs), end(pairs), std::make_tuple(1, 0)) != end(pairs));
+}
+
+TEST_CASE("CellLinkedListAgainstReference", "[cell_linked_list]") {
+    std::mt19937 generator{33};
+    using Vec3 = neighborlists::Vec<double, 3>;
+    Vec3 boxSize{10, 10, 10};
+
+    std::size_t nParticles = 1000;
+    std::vector<Vec3> positions(nParticles);
+    {
+        for (std::size_t d = 0; d < Vec3::dim; ++d) {
+            std::uniform_real_distribution<Vec3::value_type> dist{-boxSize[d] / 2, boxSize[d] / 2};
+            for (std::size_t i = 0; i < nParticles; ++i) {
+                positions[i][d] = dist(generator);
+            }
+        }
+    }
+
+    neighborlists::CellLinkedList<Vec3::dim, false, Vec3::value_type> cll{boxSize.data, 1.};
+    cll.update(begin(positions), end(positions), 3);
+    std::vector<std::tuple<int, int>> pairs{};
+    {
+        std::mutex m;
+        cll.forEachParticlePair([&pairs, &m](auto i, auto j) {
+            std::scoped_lock lock{m};
+            pairs.push_back(std::make_tuple(i, j));
+        }, 2);
+    }
+
+    for (auto i = 0U; i < nParticles; ++i) {
+        for (auto j = i + 1; j < nParticles; ++j) {
+            if ((positions[i] - positions[j]).normSquared() < 1) {
+                REQUIRE(std::find(begin(pairs), end(pairs), std::make_tuple(i, j)) != end(pairs));
+                REQUIRE(std::find(begin(pairs), end(pairs), std::make_tuple(j, i)) != end(pairs));
+            }
+        }
+    }
 }
